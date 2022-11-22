21f1a28fef85a156349705d3bf93d03565843cf3
Valery Khandryonkov <v.khandryonkov@mail.ru>
2022-11-23 01:33:42 +0300

    Parametrized one of positive tests

--- src/test/java/ru/levelp/at/homework2/LuckyTicketPositiveTest.java
+++ src/test/java/ru/levelp/at/homework2/LuckyTicketPositiveTest.java
@@ -3,12 +3,14 @@ package ru.levelp.at.homework2;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class LuckyTicketPositiveTest {
 
-    @Test
-    void sumsOfTwoHalvesAreEqual() {
-        String input = "123312";
+    @ParameterizedTest
+    @ValueSource(strings = {"000000", "999999", "123141", "009333", "579849"})
+    void sumsOfTwoHalvesAreEqual(String input) {
         boolean expected = true;
         boolean actual = LuckyTicket.isLuckyTicket(input);
         assertThat(actual).isEqualTo(expected);
